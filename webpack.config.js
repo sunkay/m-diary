46d2d2002635a7fb014d105bde69d19b2309c08d
sunkay <sunkay11@gmail.com>
2016-03-06 19:42:39 -0500

    flush out webpack config and HMR

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,76 @@
-module.exports = {
-  entry: [
-    './src/index.js'
-  ],
+const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+
+
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+  app: path.join(__dirname, 'src'),
+  build: path.join(__dirname, 'build'),
+};
+
+console.log("webpack: ", PATHS.app, PATHS.build);
+
+const common = {
+  entry: {
+    app: PATHS.app
+  },
   output: {
-    path: __dirname,
-    publicPath: '/',
+    path: PATHS.build,
     filename: 'bundle.js'
   },
   module: {
     loaders: [{
       exclude: /node_modules/,
       loader: 'babel'
+    },
+    {
+      // Test expects a RegExp! Note the slashes!
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        // Include accepts either a path or an array of paths.
+        include: PATHS.app
     }]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
-  }
 };
+
+// Default configuration
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devServer: {
+      contentBase: PATHS.build,
+
+      // Enable history API fallback so HTML5 History API based
+      // routing works. This is a good default that will come
+      // in handy in more complicated setups.
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+      devtool: 'eval-source-map',
+
+      // Display only errors to reduce the amount of output.
+      stats: 'errors-only',
+
+      // Parse host and port from env so this is easy to customize.
+      //
+      // If you use Vagrant or Cloud9, set
+      // host: process.env.HOST || '0.0.0.0';
+      //
+      // 0.0.0.0 is available to all network devices unlike default
+      // localhost
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
+
+if(TARGET === 'build') {
+  module.exports = merge(common, {});
+}

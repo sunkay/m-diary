7be483541c44a461502bcee2623f1e1e09a72f61
sunkay <sunkay11@gmail.com>
2016-05-18 20:08:02 -0400

    minor aesthetic mods

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -38,37 +38,39 @@ export class Login extends Component{
   render(){
     const { fields: { email, password }, handleSubmit } = this.props;
     return(
-      <div className="row">
-        <form onSubmit={handleSubmit(this.onSubmit.bind(this))} className="col s12">
-          <h4>LOGIN</h4>
+      <div className="container">
+        <div className="row">
+          <form onSubmit={handleSubmit(this.onSubmit.bind(this))} className="col s12">
+            <h4>LOGIN</h4>
 
-          <div className={`${email.touched && email.invalid ? 'has-danger' : '' }`}>
+            <div className={`${email.touched && email.invalid ? 'has-danger' : '' }`}>
 
-            <label htmlFor="email" >Email</label>
-            <input type="text" className="validate" {...email}/>
-            {
-              email.touched && email.error &&
-              <div><span className="red-text text-darken-2">{email.error}</span></div>
-            }
-          </div>
+              <label htmlFor="email" >Email</label>
+              <input type="text" className="validate" {...email}/>
+              {
+                email.touched && email.error &&
+                <div><span className="red-text text-darken-2">{email.error}</span></div>
+              }
+            </div>
 
-          <div className={`${password.touched && password.invalid ? 'has-danger' : '' }`}>
-            <label htmlFor="password">Password</label>
-            <input type="text" className="validate" {...password}/>
-            {
-              password.touched && password.error &&
-              <div><span className="red-text text-darken-2">{password.error}</span></div>
-            }
+            <div className={`${password.touched && password.invalid ? 'has-danger' : '' }`}>
+              <label htmlFor="password">Password</label>
+              <input type="text" className="validate" {...password}/>
+              {
+                password.touched && password.error &&
+                <div><span className="red-text text-darken-2">{password.error}</span></div>
+              }
+            </div>
+            {this.renderAlert()}
+            <div className="row">
+              <button type="submit" className="btn btn-primary">Submit</button>
+              <Link to="/" className="btn btn-danger">Cancel</Link>
+            </div>
+          </form>
+          <div className="row center">
+            <p>Don't have an account? <Link to="/signup">Sign Up</Link></p>
           </div>
-          {this.renderAlert()}
-          <div className="row">
-            <button type="submit" className="btn btn-primary">Submit</button>
-            <Link to="/" className="btn btn-danger">Cancel</Link>
-          </div>
-        </form>
-        <div className="row center">
-          <p>Don't have an account? <Link to="/signup">Sign Up</Link></p>
-        </div>
+        </div>        
       </div>
     );
   }
--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -4,41 +4,44 @@ import { Link } from 'react-router';
 export default class Signup extends Component{
   render(){
     return(
-      <div className="row signup">
-        <form className="col s12">
-          <h4>Signup</h4>
+      <div className="container">
+        <div className="row signup">
+          <form className="col s12">
+            <h4>Signup</h4>
 
-          <div className="row">
-            <div className="input-field col s12">
-              <input id="email" type="email" className="validate"/>
-              <label htmlFor="email">Email</label>
+            <div className="row">
+              <div className="input-field col s12">
+                <input id="email" type="email" className="validate"/>
+                <label htmlFor="email">Email</label>
+              </div>
             </div>
-          </div>
 
-          <div className="row">
-            <div className="input-field col s12">
-              <input id="password" type="password" className="validate"/>
-              <label htmlFor="password">Password</label>
+            <div className="row">
+              <div className="input-field col s12">
+                <input id="password" type="password" className="validate"/>
+                <label htmlFor="password">Password</label>
+              </div>
             </div>
-          </div>
 
-          <div className="row">
-            <div className="input-field col s12">
-              <input id="password_confirm" type="password" className="validate"/>
-              <label htmlFor="password">Confirm Your Password</label>
+            <div className="row">
+              <div className="input-field col s12">
+                <input id="password_confirm" type="password" className="validate"/>
+                <label htmlFor="password">Confirm Your Password</label>
+              </div>
             </div>
+
+            <div className="row">
+              <button type="submit" className="btn btn-primary">Submit</button>
+              <Link to="/" className="btn btn-danger">Cancel</Link>
+            </div>
+          </form>
+
+          <div className="row center">
+            <p>Already have an account? <Link to="/login">Sign in</Link></p>
           </div>
 
-          <div className="row">
-            <button type="submit" className="btn btn-primary">Submit</button>
-            <Link to="/" className="btn btn-danger">Cancel</Link>
-          </div>
-        </form>
-
-        <div className="row center">
-          <p>Already have an account? <Link to="/login">Sign in</Link></p>
         </div>
-
+        
       </div>
     );
   }

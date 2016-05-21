7eb036fe806bae2efa3acf6645a2893990f5ff33
sunkay <sunkay11@gmail.com>
2016-05-21 18:36:45 -0400

    testing async authn actions

--- src/actions/__test__/auth-actions-test.js
+++ src/actions/__test__/auth-actions-test.js
@@ -1,7 +1,6 @@
 import {expect} from 'chai';
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import nock from 'nock';
 import Config from '../../config';
 import {
   login, LOGIN, AUTH_USER,
@@ -11,39 +10,51 @@ import {
   thunkUser, THUNK_USER
 } from '../auth';
 
+// mock, spy
+import nock from 'nock';
+import sinon from 'sinon';
+import { hashHistory } from 'react-router';
+
+
 const middlewares = [ thunk ];
 const mockStore = configureMockStore(middlewares);
 
 describe('Auth', () => {
-  afterEach(() => {
-    nock.cleanAll();
+  var sandbox;
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
   });
 
-/*
-  it(`Test Async actions`, () => {
+  afterEach(() => {
+    nock.cleanAll();
+    sandbox.restore();
+  });
+
+  // Testing async actions with promises & stubbing hashHistory push
+  it('Login', () => {
     nock(Config.auth.url)
     .log(console.log)
-    .defaultReplyHeaders({
-      'Content-Type': 'application/json'
-    })
-    .post('/signup', {
+    .post('/signin', {
       email: 'x1@y.com',
       password: '123'
     })
-    .reply(200);
+    .reply(200, {token: 'JWT TOKEN'});
+
+    // mock the redirect request in the action
+    var push = sandbox.stub(hashHistory, 'push');
 
     const expectedActions = [
-      {type: 'AUTH_USER'},
-      {type: 'AUTH_USE'}
-    ];
+      {
+        type: 'AUTH_USER'
+       }
+    ]
+    const store = mockStore({ });
 
-    const store = mockStore({});
-    return store.dispatch(thunkUser({email:'x1@y.com', password:'123'}))
+    return store.dispatch(login({email: 'x1@y.com', password: '123'}))
     .then(() => {
-      expect(store.getActions()).to.deep.equal(expectedActions);
-    })
-  });
-*/
+      expect(store.getActions()).to.deep.equal(expectedActions)
+    });
+
 
   it('Signup User', () => {
     nock(Config.auth.url)
@@ -54,11 +65,8 @@ describe('Auth', () => {
     })
     .reply(200, { token: 'JWT TOKEN'});
 
-    nock('http://localhost:8080')
-    .log(console.log)
-    .get('/#/')
-    .reply(200);
-
+    // mock the redirect request in the action
+    var push = sandbox.stub(hashHistory, 'push');
 
     const expectedActions = [
       {
@@ -69,13 +77,13 @@ describe('Auth', () => {
 
     return store.dispatch(signupUser({email: 'x1@y.com', password: '123'}))
     .then(() => {
-      console.log(store.getActions());
       expect(store.getActions()).to.deep.equal(expectedActions)
     });
 
   });
 
-/*
+  });
+
   it('Test redux-mock-store', () => {
     const store = mockStore({});
     const dummyAction = {type: 'dummy'};
@@ -94,9 +102,5 @@ describe('Auth', () => {
     expect(authError('error').payload).to.equal('error')
   });
 
-  it('SIGNUP_USER', () => {
-    // need to figure out how to test async action dispatch functions
-  });
-*/
 
 });
--- src/actions/auth.js
+++ src/actions/auth.js
@@ -7,26 +7,19 @@ export const AUTH_USER = "AUTH_USER";
 export function login({email, password}, url){
   return function(dispatch){
     // sign in with the auth server
-    axios.post(`${Config.auth.url}/signin`, {email, password})
+    return axios.post(`${Config.auth.url}/signin`, {email, password})
       .then(response => {
-        console.log("enter");
         // store the token in localstorage
         localStorage.setItem('token', response.data.token);
 
         // Update state to indicate authenticated user
-        console.log("dispatch AUTH_USER");
         dispatch({type: AUTH_USER});
 
         // redirect to the right url
         console.log('redirecting to... ', url);
         hashHistory.push(url);
-        console.log("exit");
-
       })
       .catch((response) => {
-        console.log("error enter");
-
-        console.log("Error signing in... ", response.data);
         dispatch(authError('The email address or password you entered is incorrect.'));
       });
 
@@ -40,7 +33,7 @@ export function signupUser({email, password}){
     return axios.post(`${Config.auth.url}/signup`, {email, password})
       .then(response => {
         // store the token in localstorage
-        console.log("signed up: ", response.data.token);
+        //console.log("signed up: ", response.data.token);
         localStorage.setItem('token', response.data.token);
 
         // Update state to indicate authenticated user
@@ -51,28 +44,10 @@ export function signupUser({email, password}){
         hashHistory.push('/');
       })
       .catch((response) => {
-        console.log("in Error: ", response.data)
         dispatch(authError(`Error signing up the user: ${response.data.error}`));
       });
   }
 }
-
-export const THUNK_USER = "THUNK_USER";
-export function thunkUser({email, password}){
-  return function(dispatch){
-    return axios.post(`${Config.auth.url}/signup`, {email, password})
-      .then(response => {
-        console.log("in then", response.data);
-        dispatch({type: 'AUTH_USER'});
-        dispatch({type: 'AUTH_USER'});
-      })
-      .catch((response) => {
-        console.log("in error", response.data);
-        dispatch({type: 'AUTH_ERROR'});
-      });
-  }
-}
-
 
 export const UNAUTH_USER = "UNAUTH_USER";
 export function logout(){
